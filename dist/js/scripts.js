e9f110b59fadaac27ff249c1f73a5e90385e7c82
marcin-bojar <marcin.bojar@interia.pl>
2021-05-16 15:20:55 +0200

    Customized mobile and desktop nav.

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,36 +1,39 @@
 /*!
-* Start Bootstrap - Border-Pass v1.0.0 (undefined)
-* Copyright 2013-2021 BITS Marcin Bojar
-* Licensed under undefined (https://github.com/StartBootstrap/undefined/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Border-Pass v1.0.0 (undefined)
+ * Copyright 2013-2021 BITS Marcin Bojar
+ * Licensed under undefined (https://github.com/StartBootstrap/undefined/blob/master/LICENSE)
+ */
 (function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using anime.js
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-            this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length ?
-                target :
-                $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                anime({
-                    targets: 'html, body',
-                    scrollTop: target.offset().top - 54,
-                    duration: 1000,
-                    easing: 'easeInOutExpo'
-                });
-                return false;
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on(
+        "click",
+        function () {
+            if (
+                location.pathname.replace(/^\//, "") ==
+                    this.pathname.replace(/^\//, "") &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length
+                    ? target
+                    : $("[name=" + this.hash.slice(1) + "]");
+                if (target.length) {
+                    anime({
+                        targets: "html, body",
+                        scrollTop: target.offset().top - 54,
+                        duration: 1000,
+                        easing: "easeInOutExpo",
+                    });
+                    return false;
+                }
             }
         }
-    });
+    );
 
     // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").on('click', function () {
+    $(".js-scroll-trigger").on("click", function () {
         $(".navbar-collapse").collapse("hide");
     });
 
@@ -42,7 +45,9 @@
 
     // Collapse Navbar
     var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
+        var isMenuOpen = $("#navbarResponsive").hasClass("show");
+
+        if ($("#mainNav").offset().top > 100 || isMenuOpen) {
             $("#mainNav").addClass("navbar-shrink");
         } else {
             $("#mainNav").removeClass("navbar-shrink");
@@ -51,6 +56,17 @@
     // Collapse now if page is not at top
     navbarCollapse();
     // Collapse the navbar when page is scrolled
-    $(window).on('scroll', navbarCollapse);
+    $(window).on("scroll", navbarCollapse);
 
+    $(".navbar-toggler").on("click", function () {
+        var isMenuOpen = $("#navbarResponsive").hasClass("show");
+        if (!isMenuOpen) {
+            $(".navbar.navbar-expand-lg").addClass("navbar-shrink");
+        } else {
+            if ($("#mainNav").offset().top < 100)
+                setTimeout(function () {
+                    $(".navbar.navbar-expand-lg").removeClass("navbar-shrink");
+                }, 245);
+        }
+    });
 })(jQuery); // End of use strict
